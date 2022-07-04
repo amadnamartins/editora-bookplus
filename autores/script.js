f3e2475d661f8355940580e3736ce58dfe6cf82b
amadnamartins <amandacarolinamartins@hotmail.com>
2022-07-04 17:03:26 -0300

    Initial commit

---
+++ autores/script.js
@@ -0,0 +1,41 @@
+
+
+const menuBtn = document.querySelector(".menu-btn");
+    const navigation = document.querySelector(".navigation");
+
+    menuBtn.addEventListener("click", () => {
+      menuBtn.classList.toggle("active");
+      navigation.classList.toggle("active");
+    });
+
+        //Javascript for video slider navigation -> 
+        //Javascript para um menu de navegação responsivo
+
+        const btns = document.querySelectorAll(".nav-btn");
+        const slides = document.querySelectorAll(".video-slide");
+        const contents = document.querySelectorAll(".content");
+        
+        let sliderNav = function(manual){
+            
+            btns.forEach((btn) =>{
+            btn.classList.remove("active")
+            })
+
+            slides.forEach((slide) =>{
+            slide.classList.remove("active")
+            })
+
+            contents.forEach((slide) =>{
+            slide.classList.remove("active")
+            })
+
+            btns[manual].classList.add("active");
+            slides[manual].classList.add("active");
+            contents[manual].classList.add("active");
+            }
+
+        btns.forEach((btn, i) =>{
+            btn.addEventListener("click", () => {
+            sliderNav(i)
+            })
+        })
